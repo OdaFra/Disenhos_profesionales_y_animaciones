80b1cbc82ff317e9ccdb5b3cbdcc542030a863f5
OdaFra <odafra@OdaFra.local>
2022-04-29 16:07:23 -0400

    Se agrega page animaciones

--- lib/src/pages/header_page.dart
+++ lib/src/pages/header_page.dart
@@ -1,11 +1,11 @@
-import 'package:disenos_app/src/widgets/header.dart';
+import 'package:disenos_app/src/pages/animaciones_page.dart';
 import 'package:flutter/material.dart';
 
 class HeardesPage extends StatelessWidget {
-  const HeardesPage({Key key}) : super(key: key);
+  const HeardesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: HeaderWaveGradiente());
+    return Scaffold(body: AnimacionesPage());
   }
 }
