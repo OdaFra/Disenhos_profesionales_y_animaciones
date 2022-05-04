64756c28e2a56e15e0a5edd31171f71b7db0ada4
OdaFra <odafra@OdaFra.local>
2022-05-04 17:11:33 -0400

    Se agrega variables

--- lib/src/widgets/radialProgress.dart
+++ lib/src/widgets/radialProgress.dart
@@ -11,17 +11,47 @@ class RadialProgress extends StatefulWidget {
   State<RadialProgress> createState() => _RadialProgressState();
 }
 
-class _RadialProgressState extends State<RadialProgress> {
+class _RadialProgressState extends State<RadialProgress>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late double porcentajeAnterior;
+
+  @override
+  void initState() {
+    porcentajeAnterior = widget.porcentaje;
+    controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 200));
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      width: double.infinity,
-      height: double.infinity,
-      child: CustomPaint(
-        painter: _MiRadialProgess(widget.porcentaje),
-      ),
-    );
+    controller.forward(from: 0.0);
+    final diferenciaAnimar = widget.porcentaje - porcentajeAnterior;
+    porcentajeAnterior = widget.porcentaje;
+    return AnimatedBuilder(
+        animation: controller,
+        builder: (BuildContext context, Widget? child) {
+          return Container(
+            padding: const EdgeInsets.all(10),
+            width: double.infinity,
+            height: double.infinity,
+            child: CustomPaint(
+              painter: _MiRadialProgess(
+                (widget.porcentaje - diferenciaAnimar) +
+                    (diferenciaAnimar * controller.value),
+              ),
+            ),
+          );
+        });
   }
 }
 
