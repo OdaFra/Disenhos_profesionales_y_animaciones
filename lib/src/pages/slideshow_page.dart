3014dae0feaf6ad330f978ff9c2f83ecc1e538f4
Oscar Ramirez <oscar.ramirez@itti.digital>
2022-05-06 09:46:30 -0400

    Optimizaciones para crear list de SlideShow

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../widgets/slideShow.dart';
 
 class SlideshowPage extends StatelessWidget {
   const SlideshowPage({ Key? key }) : super(key: key);
@@ -6,7 +9,15 @@ class SlideshowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Hola Mundo')),
+      body: Slideshow(
+        slides: [
+          SvgPicture.asset('assets/svgs/slide-1.svg'),
+          SvgPicture.asset('assets/svgs/slide-2.svg'),
+          SvgPicture.asset('assets/svgs/slide-3.svg'),
+          SvgPicture.asset('assets/svgs/slide-4.svg'),
+          SvgPicture.asset('assets/svgs/slide-5.svg')
+        ],
+      )
         );
   }
 }
--- lib/src/widgets/slideShow.dart
+++ lib/src/widgets/slideShow.dart
@@ -1,16 +1,138 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import '../models/slider_model.dart';
+
+
+
 
 class Slideshow extends StatelessWidget {
 
+  final List<Widget> slides;
+
+   Slideshow({required  this.slides});
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-  create: (_) => SliderModel(),
-      
+      create: (_) => SliderModel(),
+      child: Center(
+          child: Column(
+        children:  [
+          Expanded(
+            child: _Slides(this.slides ),
+          ),
+          _Dots()
+        ],
+      )),
+    );
+  }
+}
+
+class _Slides extends StatefulWidget {
+  final List<Widget>slides;
+  const _Slides(this.slides);
+
+  @override
+  State<_Slides> createState() => _SlidesState();
+}
+
+class _SlidesState extends State<_Slides> {
+  final pageViewController = PageController();
+
+  @override
+  void initState() {
+    pageViewController.addListener(() {
+      print('Pagina actual:  ${pageViewController.page}');
+      //Actualizar la instanacia de mi clase o provider (sliderModel)
+      Provider.of<SliderModel>(context, listen: false).currentPage =
+          pageViewController.page!;
+
+
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageViewController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+
+        controller: pageViewController,
+        // children: 
+        // const [
+        //   _Slide('assets/svgs/slide-1.svg'),
+        //   _Slide('assets/svgs/slide-2.svg'),
+        //   _Slide('assets/svgs/slide-3.svg'),
+        // ],
+        children: widget.slides.map((slide) => _Slide(slide)).toList(),
+      ),
+    );
+  }
+}
+
+class _Dots extends StatelessWidget {
+  const _Dots({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          _Dot(0),
+          _Dot(1),
+          _Dot(2),
+        ],
+      ),
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+  final int index;
+  const _Dot(
+    this.index,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final pageViewIndex = Provider.of<SliderModel>(context).currentPage;
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      width: 12,
+      height: 12,
+      margin: const EdgeInsets.symmetric(horizontal: 5),
+      decoration: BoxDecoration(
+        color: (pageViewIndex >= index - 0.5 && pageViewIndex < index + 0.5)
+            ? Colors.blue
+            : Colors.grey,
+        shape: BoxShape.circle,
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  // final String svg;
+  final Widget slide;
+
+  const _Slide(this.slide);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: const EdgeInsets.all(30),
+      child: slide,
     );
   }
 }
