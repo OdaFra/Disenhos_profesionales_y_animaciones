15497713ecdf9ea89d04e61d660e83f595111605
Oscar Ramirez <oscar.ramirez@itti.digital>
2022-05-09 10:35:13 -0400

    Se agrega expanded para redibujar el slider en un widget de columm

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -10,20 +10,34 @@ class SlideshowPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       // backgroundColor: Colors.indigo,
-      body: Slideshow(
-          bulletPrimario:15,
-          bulletSecundario:12,
-     // puntosArriba: false,
-         colorPrimario:const Color(0xffFF5A7E),
-         //colorSecundario: Colors.blue.shade50,
-        slides: [
-          SvgPicture.asset('assets/svgs/slide-1.svg'),
-          SvgPicture.asset('assets/svgs/slide-2.svg'),
-          SvgPicture.asset('assets/svgs/slide-3.svg'),
-          SvgPicture.asset('assets/svgs/slide-4.svg'),
-          SvgPicture.asset('assets/svgs/slide-5.svg')
+      body: Column(
+        children: [
+          Expanded(child: MiSlideShow()),
+           Expanded(child: MiSlideShow()),
         ],
       )
         );
+  }
+}
+
+class MiSlideShow extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Slideshow(
+        bulletPrimario:15,
+        bulletSecundario:12,
+     // puntosArriba: false,
+       colorPrimario:const Color(0xffFF5A7E),
+       //colorSecundario: Colors.blue.shade50,
+      slides: [
+        SvgPicture.asset('assets/svgs/slide-1.svg'),
+        SvgPicture.asset('assets/svgs/slide-2.svg'),
+        SvgPicture.asset('assets/svgs/slide-3.svg'),
+        SvgPicture.asset('assets/svgs/slide-4.svg'),
+        SvgPicture.asset('assets/svgs/slide-5.svg')
+      ],
+    );
   }
 }
