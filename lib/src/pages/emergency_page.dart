c281a6d3ffd5a6989fc2b731d450c5f347b40b82
Oscar Ramirez <oscar.ramirez@itti.digital>
2022-05-11 14:35:04 -0400

    Se agrega parametros para seleccionar, el color, icon y texto

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -1,3 +1,4 @@
+import 'package:disenos_app/src/widgets/button_gordo.dart';
 import 'package:disenos_app/src/widgets/header.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -7,8 +8,16 @@ class EmergencyPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body:_HeaderPage()
+    return  Scaffold(
+      // backgroundColor: Colors.red,
+      body:Center(child: ButtonGordoPage(
+        texto: 'Motor Accident',
+        onPress: (){ print('Click!!!');},
+        icon: FontAwesomeIcons.carCrash,
+        color1: Color(0xff6989f5),
+             color2:Color(0xff906ef5)
+
+      ))
    );
   }
 }
